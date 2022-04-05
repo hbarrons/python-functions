30a1f6a028dcc0db1914659d910d630ca603d79a
hbarrons <hbarrons@gmail.com>
2022-04-04 21:43:38 -0400

    solve problem 2

--- python-functions.py
+++ python-functions.py
@@ -1,8 +1,17 @@
-def sum_to(num):
-  sum = 0
-  for n in range(1, num + 1):
-   sum += n
-  return sum
+# def sum_to(num):
+#   sum = 0
+#   for n in range(1, num + 1):
+#    sum += n
+#   return sum
 
-print(sum_to(6))
-print(sum_to(10))
+# print(sum_to(6))
+# print(sum_to(10))
+
+
+
+def largest(list):
+  list.sort()
+  print(list[-1])
+
+largest([1, 2, 3, 4, 0]) 
+largest([10, 4, 2, 231, 91, 54])  
